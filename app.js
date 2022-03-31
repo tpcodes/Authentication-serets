7ce34ac956d5fea584d6a350af30b9ab7e51d18e
Tushar Pandey <tusharpandey586@gmail.com>
2022-04-01 00:29:44 +0530

    level e hashing passwords added md5

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose= require("mongoose");
-const encrypt= require("mongoose-encryption");
+const md5= require("md5");
+// const encrypt= require("mongoose-encryption");
 
 
 
@@ -24,7 +25,7 @@ const userSchema=new mongoose.Schema(
 );
 //encryption 
 // console.log(process.env.SECRET);
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields: ['password']});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields: ['password']});
 
 
 
@@ -50,7 +51,7 @@ app.post("/register",function(req,res)
     const newUser=new User(
         {
             email:email,
-            password:password
+            password:md5(password)
         }
     )
     
@@ -68,7 +69,7 @@ app.post("/register",function(req,res)
 app.post("/login",function(req,res)
 {
     const email=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     User.findOne({email:email},function(err,foundedData)
     {
